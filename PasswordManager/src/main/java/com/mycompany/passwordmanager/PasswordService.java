d0a1e8fe4f0618bc9646dffbda0fed5752b50e90
Chaila1 <115548186+Chaila1@users.noreply.github.com>
2024-11-12 14:34:03 +0000

    password encryption

--- PasswordManager/src/main/java/com/mycompany/passwordmanager/PasswordService.java
+++ PasswordManager/src/main/java/com/mycompany/passwordmanager/PasswordService.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.passwordmanager;
 
 import java.security.MessageDigest;
@@ -21,13 +20,31 @@ public class PasswordService {
     // Using a static map to store users and their hashed passwords
     private static final HashMap<String, String> users = new HashMap<>();
     private SecretKey key;
-    
-        public PasswordService() {
+    static Cipher ci;
+    private static String website, password, name, email;
+
+    public PasswordService(String website, String password) {
+        this.website = website;
+        this.password = password;
+    }
+
+    public PasswordService() throws Exception {
         try {
             // Generate a Secret Key for AES encryption
             KeyGenerator keyGen = KeyGenerator.getInstance("AES");
             keyGen.init(128);
             this.key = keyGen.generateKey();
+
+            ci = Cipher.getInstance("AES"); //SunJCE provider AES algorithm, mode(optional) and padding schema(optional)  
+
+            String plainText = "AES Symmetric Encryption Decryption";
+            System.out.println("Plain Text Before Encryption: " + plainText);
+
+            String encryptedText = encrypt(plainText, key);
+            System.out.println("Encrypted Text After Encryption: " + encryptedText);
+
+            String decryptedText = decrypt(encryptedText, key);
+            System.out.println("Decrypted Text After Decryption: " + decryptedText);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
@@ -65,36 +82,56 @@ public class PasswordService {
         scanner.close();
     }
 
-    private static void savePasword(Scanner scanner){
+    private static void savePasword(Scanner scanner) {
         System.out.print("Enter website: ");
-        String website = scanner.nextLine();
+        website = scanner.nextLine();
 
         System.out.print("Enter password: ");
-        String password = scanner.nextLine();
+        password = scanner.nextLine();
 
         String hashedPassword = hashPassword(password);
-        
-        
+
         users.put(website, hashedPassword);
-        
+
         System.out.println("User registered successfully.");
     }
 
     private static void retrievePassword(Scanner scanner) {
         System.out.print("Enter website: ");
-        String website = scanner.nextLine();
+        website = scanner.nextLine();
 
         System.out.print("Enter password: ");
-        String password = scanner.nextLine();
+        password = scanner.nextLine();
 
         String hashedPassword = hashPassword(password);
 
         if (users.containsKey(website) && users.get(website).equals(hashedPassword)) {
             System.out.println("Login successful.");
-            System.out.println("Password: "+ password + "\n" + "Hashed password: " + hashedPassword);
+            System.out.println("Password: " + password + "\n" + "Hashed password: " + hashedPassword);
         } else {
             System.out.println("Invalid username or password.");
         }
+    }
+
+    public static String encrypt(String plainText, SecretKey secretKey)
+            throws Exception {
+        plainText = password;
+        byte[] plainTextByte = plainText.getBytes();
+        ci.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedByte = ci.doFinal(plainTextByte);
+        Base64.Encoder encoder = Base64.getEncoder();
+        String encryptedText = encoder.encodeToString(encryptedByte);
+        return encryptedText;
+    }
+
+    public static String decrypt(String encryptedText, SecretKey secretKey)
+            throws Exception {
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] encryptedTextByte = decoder.decode(encryptedText);
+        ci.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedByte = ci.doFinal(encryptedTextByte);
+        String decryptedText = new String(decryptedByte);
+        return decryptedText;
     }
 
     private static String hashPassword(String password) {
@@ -111,5 +148,4 @@ public class PasswordService {
             return null;
         }
     }
-    
 }
